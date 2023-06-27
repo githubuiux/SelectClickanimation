7226df296f00fc89b65ccd4aa1bdc3ac68e151c7
githubuiux <uiuxista@gmail.com>
2023-06-27 12:18:02 +0300

    Initial Commit

---
+++ SelectClickanimation/SelectClickanimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectClickanimationApp.swift
+//  SelectClickanimation
+//
+//  Created by Levent on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SelectClickanimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
